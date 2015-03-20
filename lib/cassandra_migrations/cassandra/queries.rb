a5bc551e56534a86f289b6611326a78094de9be7
sstgithub <sstantia@gmail.com>
2015-03-20 10:12:46 -0700

    fixes get_column_type and allows user to enter secondary options as second hash within options

--- lib/cassandra_migrations/cassandra/queries.rb
+++ lib/cassandra_migrations/cassandra/queries.rb
@@ -39,32 +39,30 @@ module CassandraMigrations
         execute(query)
       end
 
-      def select(table, statement_options={}, secondary_options=nil)
-        query_string = "SELECT #{statement_options[:projection] || '*'} FROM #{table}"
+      def select(table, options={})
+        query_string = "SELECT #{options[:projection] || '*'} FROM #{table}"
 
-        if statement_options[:selection]
-          query_string << " WHERE #{statement_options[:selection]}"
+        if options[:selection]
+          query_string << " WHERE #{options[:selection]}"
         end
 
-        if statement_options[:order_by]
-          query_string << " ORDER BY #{statement_options[:order_by]}"
+        if options[:order_by]
+          query_string << " ORDER BY #{options[:order_by]}"
         end
 
-        if statement_options[:limit]
-          query_string << " LIMIT #{statement_options[:limit]}"
+        if options[:limit]
+          query_string << " LIMIT #{options[:limit]}"
         end
 
-        if statement_options[:allow_filtering]
+        if options[:allow_filtering]
           query_string << " ALLOW FILTERING"
         end
 
-        if secondary_options
-          execute(query_string)
+        if options[:secondary_options]
+          execute(query_string, options[:secondary_options])
         else
-          execute(query_string, secondary_options)
+          execute(query_string)
         end
-
-
       end
 
       def delete!(table, selection, options={})
@@ -78,28 +76,28 @@ module CassandraMigrations
 
     private
 
+      SYMBOL_FOR_TYPE = {
+        'SetType' => :set,
+        'ListType' => :list,
+        'MapType' => :map,
+        'BooleanType' => :boolean,
+        'FloatType' => :float,
+        'Int32Type' => :int,
+        'DateType' => :timestamp,
+        'UTF8Type' => :string,
+        'BytesType' => :blob,
+        'UUIDType' => :uuid,
+        'DoubleType' => :double,
+        'InetAddressType' => :inet,
+        'AsciiType' => :ascii,
+        'LongType' => :bigint ,
+        'DecimalType' => :decimal,
+        'TimeUUIDType' => :timeuuid
+      }
+
       def get_column_type(table, column)
         column_info = client.execute("SELECT VALIDATOR FROM system.schema_columns WHERE keyspace_name = '#{client.keyspace}' AND columnfamily_name = '#{table}' AND column_name = '#{column}'")
-        raw_type = column_info.first['validator']
-
-        case
-          when raw_type.include?('SetType'); :set
-          when raw_type.include?('ListType'); :list
-          when raw_type.include?('MapType'); :map
-          when raw_type.include?('BooleanType'); :boolean
-          when raw_type.include?('FloatType'); :float
-          when raw_type.include?('Int32Type'); :int
-          when raw_type.include?('DateType'); :timestamp
-          when raw_type.include?('UTF8Type'); :string
-          when raw_type.include?('BytesType'); :blob
-          when raw_type.include?('UUIDType'); :uuid
-          when raw_type.include?('DoubleType'); :double
-          when raw_type.include?('InetAddressType'); :inet
-          when raw_type.include?('AsciiType'); :ascii
-          when raw_type.include?('LongType'); :bigint
-          when raw_type.include?('DecimalType'); :decimal
-          when raw_type.include?('TimeUUIDType'); :timeuuid
-        end
+        SYMBOL_FOR_TYPE[column_info.first['validator'].split(',').first.split('.')[-1]]
       end
 
       def to_cql_value(column, value, table, options={})
@@ -107,20 +105,13 @@ module CassandraMigrations
         operation = operator ? "#{column} #{operator} " : ''
 
         if value.respond_to?(:strftime)
-          "'#{value.strftime('%Y-%m-%d %H:%M:%S%z')}'"
+          datetime_to_cql(value)
         elsif value.is_a?(String)
-          "'#{value}'"
+          string_to_cql(value)
         elsif value.is_a?(Array)
-          type = get_column_type(table, column)
-          values = %[#{value.map { |v| to_cql_value(nil, v, nil) } * ', '}]
-
-          if type && type == :list
-            %[#{operation}[#{values}]]
-          else # it must be a set!
-            %[#{operation}{#{values}}]
-          end
+          array_value_to_cql(column, value, table, operation)
         elsif value.is_a?(Hash)
-          "#{operation}{ #{value.reduce([]) {|sum, (key, value)| sum << "'#{key}': '#{value}'" }.join(", ") } }"
+          hash_to_cql(value, operation)
         else
           value = value.to_s
           if value == ""
